5dce43eca862666432ea97e7b26d17ad147a3d9b
Prince <princemwill@users.noreply.github.com>
2016-07-13 09:29:44 -0400

    First 4 from challenge #3

--- selectbasics.sql
+++ selectbasics.sql
@@ -45,7 +45,20 @@ SELECT name FROM world
 -- #10
 SELECT name FROM world
  WHERE name LIKE '____'
-  
+
+
+-- SELECT basics - challenge 3
+-- #1
+SELECT name, continent, population FROM world;
+-- #2
+SELECT name FROM world
+WHERE population>200000000;
+-- #3
+SELECT name, gdp/population FROM world
+WHERE population>200000000;
+-- #4
+SELECT name, population/1000000  FROM world WHERE continent = 'South America';
+
   
   
   
