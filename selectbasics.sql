5166f38bc544b07396d5f870a3b90aeb05ba180f
Prince <princemwill@users.noreply.github.com>
2016-07-13 09:56:54 -0400

    first 8 of challenge 3 done

--- selectbasics.sql
+++ selectbasics.sql
@@ -58,6 +58,13 @@ SELECT name, gdp/population FROM world
 WHERE population>200000000;
 -- #4
 SELECT name, population/1000000  FROM world WHERE continent = 'South America';
+-- #5
+SELECT name, population FROM world WHERE name ='France' OR name ='Germany' OR name ='Italy';
+-- #6
+SELECT name FROM world WHERE name LIKE '%United%';
+-- #7
+SELECT name, population, area FROM world WHERE area>3000000 OR population>250000000;
+-- #8
 
   
   
