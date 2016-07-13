092eae5c9b00fcfe326e1a837a1371e352e45f44
Prince <princemwill@users.noreply.github.com>
2016-07-13 08:58:32 -0400

    Challenge #1 complete for real now...

--- selectbasics.sql
+++ selectbasics.sql
@@ -1,10 +1,22 @@
 
 -- SELECT basics - challenge 1
+
+-- #1
 SELECT population FROM world
 WHERE name = 'Germany';
+-- #2
+SELECT name, population FROM world
+  WHERE name IN ('Ireland', 'Iceland', 'Denmark');
+-- #3
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000; 
+  
+  
 
 -- SELECT basics - challenge 2
 SELECT name FROM world
   WHERE name LIKE 'Y%'
+  
+  
   
   
