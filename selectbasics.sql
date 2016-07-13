67efee694a66824015a2a205a5839c6d8bb45bc5
Prince <princemwill@users.noreply.github.com>
2016-07-13 09:13:05 -0400

    first 7 of 2nd challenge

--- selectbasics.sql
+++ selectbasics.sql
@@ -14,8 +14,29 @@ SELECT name, area FROM world
   
 
 -- SELECT basics - challenge 2
+-- #1
 SELECT name FROM world
   WHERE name LIKE 'Y%'
+-- #2 
+SELECT name FROM world
+  WHERE name LIKE '%y'
+-- #3
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+-- #4
+SELECT name FROM world
+  WHERE name LIKE '%land%'
+-- #5
+SELECT name FROM world
+  WHERE name LIKE 'C%ia%'
+-- #6
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+-- #7
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+  
+  
   
   
   
