30de09fd8d1efad0fb5dce3bfdb9ff2ec43d8527
Prince <princemwill@users.noreply.github.com>
2016-07-13 10:45:12 -0400

    All other challenges moved to their own files

--- selectbasics.sql
+++ selectbasics.sql
@@ -1,5 +1,5 @@
 
--- SELECT basics - challenge 1
+-- 0 SELECT basics - challenge 1
 
 -- #1
 SELECT population FROM world
@@ -11,65 +11,9 @@ SELECT name, population FROM world
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000; 
   
-  
-
--- SELECT basics - challenge 2
--- #1
-SELECT name FROM world
-  WHERE name LIKE 'Y%'
--- #2 
-SELECT name FROM world
-  WHERE name LIKE '%y'
--- #3
-SELECT name FROM world
-  WHERE name LIKE '%x%'
--- #4
-SELECT name FROM world
-  WHERE name LIKE '%land%'
--- #5
-SELECT name FROM world
-  WHERE name LIKE 'C%ia%'
--- #6
-SELECT name FROM world
-  WHERE name LIKE '%oo%'
--- #7
-SELECT name FROM world
-  WHERE name LIKE '%a%a%a%'
--- #8
-SELECT name FROM world
- WHERE name LIKE '_t%'
-ORDER BY name
--- #9
-SELECT name FROM world
- WHERE name LIKE '%o__o%'
--- #10
-SELECT name FROM world
- WHERE name LIKE '____'
 
 
--- SELECT basics - challenge 3
--- #1
-SELECT name, continent, population FROM world;
--- #2
-SELECT name FROM world
-WHERE population>200000000;
--- #3
-SELECT name, gdp/population FROM world
-WHERE population>200000000;
--- #4
-SELECT name, population/1000000  FROM world WHERE continent = 'South America';
--- #5
-SELECT name, population FROM world WHERE name ='France' OR name ='Germany' OR name ='Italy';
--- #6
-SELECT name FROM world WHERE name LIKE '%United%';
--- #7
-SELECT name, population, area FROM world WHERE area>3000000 OR population>250000000;
--- #8
-SELECT name, population, area FROM world WHERE area>3000000 XOR population>250000000
--- #9
-  SELECT name, ROUND(population/1000000,2), ROUND(GDP/1000000000,2) FROM world WHERE continent = 'South America';
--- #10
-SELECT name, ROUND(gdp/population,-3) FROM world WHERE GDP >= 1000000000000;
+
   
   
   
