684df40481708b1d86e6a7e2ad6c5fcf0b2e21da
Prince <princemwill@users.noreply.github.com>
2016-07-13 09:18:25 -0400

    2nd Challenge complete for real

--- selectbasics.sql
+++ selectbasics.sql
@@ -35,6 +35,16 @@ SELECT name FROM world
 -- #7
 SELECT name FROM world
   WHERE name LIKE '%a%a%a%'
+-- #8
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+-- #9
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+-- #10
+SELECT name FROM world
+ WHERE name LIKE '____'
   
   
   
